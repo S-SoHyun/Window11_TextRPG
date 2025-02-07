87f7aee53be65d0d80a8b896e1ef1d98bb7a67dc
ProgramCnt <corfafa2@naver.com>
2025-02-07 10:25:13 +0900

    [Feat] 몬스터 클래스 추가

---
+++ Window11_TextRPG/Monster.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window11_TextRPG
+{
+    public class Monster
+    {
+        string name;
+        int level;
+        string type;
+        int hp;
+
+        public Monster(string name, int level, string type, int hp) 
+        {
+            this.name = name;
+            this.level = level;
+            this.type = type;
+            this.hp = hp;
+        }
+
+        //hp 0 이하인지 체크
+        public bool IsDie()
+        {
+            if (hp <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
