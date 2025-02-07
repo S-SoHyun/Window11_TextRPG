0fb54bc0487a0f99be801567b24ade2ddbd250de
Lee Sanghun <hun1387s@gmail.com>
2025-02-07 10:17:26 +0900

    [feat] 아이템 구조 작성
    - enum ITEMTYPE
    - Item 추상클래스
    - Item 상속 받는 MountableItem
    - Item 상속 받는 PotionItem

---
+++ Window11_TextRPG/Item.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Window_11_TEXTRPG
+{
+    enum ITEMTYPE
+    {
+        WEAPON,
+        ARMOR,
+        POTION,
+    }
+    internal abstract class Item
+    {
+        protected string? name;
+        protected int price;
+        protected ITEMTYPE type;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public ITEMTYPE Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+    }
+
+    // 장착 가능한 아이템
+    class MountableItem : Item
+    {
+        private int attack;
+        private int defense;
+        private bool own;
+        private bool equip;
+
+        public int Attack
+        {
+            get { return attack; }
+            set { attack = value; }
+        }
+        public int Defense
+        {
+            get { return defense; }
+            set { defense = value; }
+        }
+        public bool Own
+        {
+            get { return own; }
+            set { own = value; }
+        }
+        public bool Equip
+        {
+            get { return equip; }
+            set { equip = value; }
+        }
+
+    }
+
+    // 포션 아이템
+    class PotionItem : Item
+    {
+        private int count;
+        private int heel;
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+        public int Heel
+        {
+            get { return heel; }
+            set { heel = value; }
+        }
+    }
+
+
+
+
+    
+}
