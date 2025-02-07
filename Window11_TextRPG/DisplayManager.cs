f3166956168682a312fc2f4794e2ee4664a030e7
Lee Sanghun <hun1387s@gmail.com>
2025-02-07 11:51:36 +0900

    [feat] Store Buy Sell 화면 출력 구성

--- Window11_TextRPG/DisplayManager.cs
+++ Window11_TextRPG/DisplayManager.cs
@@ -152,19 +152,131 @@ namespace Window11_TextRPG
             InputInduction();
         }
 
-        public static void StoreScene(Player plyaer, List<Item> items)
+        public static void StoreBuyScene(Player plyaer, List<MountableItem> items)
         {
+            Console.Write("[상점 - 구매]");
 
+            AddBlankLine();
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+
+            AddBlankLine();
+            Console.WriteLine("[보유골드]");
+            Console.WriteLine($"{plyaer.Gold} G");
+
+            AddBlankLine();
+            Console.WriteLine("[아이템 목록]");
+
+            printBuyItem(items);
+
+            AddBlankLine(2);
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("2. 아이템 판매");
+
+            AddBlankLine();
+            Console.WriteLine("0. 나가기");
+
+            InputInduction();
         }
 
-        public static void StoreBuyScene(Player plyaer, List<Item> items)
+        public static void StoreSellScene(Player plyaer, List<MountableItem> items)
         {
+            Console.Write("[상점 - 판매]");
 
+            AddBlankLine();
+            Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+
+            AddBlankLine();
+            Console.WriteLine("[보유골드]");
+            Console.WriteLine($"{plyaer.Gold} G");
+
+            AddBlankLine();
+            Console.WriteLine("[아이템 목록]");
+
+            printSellItem(items);
+
+            AddBlankLine(2);
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("2. 아이템 판매");
+
+            AddBlankLine();
+            Console.WriteLine("0. 나가기");
+
+            InputInduction();
         }
 
-        public static void StoreSellScene(Player plyaer, List<Item> items)
+        private static void printBuyItem(List<MountableItem> items)
         {
+            // item 출력
+            int count = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                // 장비 장착 옵션
+                string equip = "구매완료";
+                // 공격력 or 방어력
+                string AorD = "";
 
+                {
+                    // 방어 장비
+                    if (items[i].Defense > 0)
+                    {
+                        AorD = $"방어력 +{items[i].Defense}";
+                    }
+                    // 공격 장비
+                    else if (items[i].Attack > 0)
+                    {
+                        AorD = $"공격력 +{items[i].Attack}";
+                    }
+                    count++;
+                    if (items[i].Own)
+                    {
+                        Console.WriteLine($" -{count} {items[i].Name} | {AorD} | {items[i].Description} | {equip}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($" -{count} {items[i].Name} | {AorD} | {items[i].Description} | {items[i].Gold} G");
+                    }
+
+
+                }
+            }
+        }
+
+        private static void printSellItem(List<MountableItem> items)
+        {
+            // item 출력
+            int count = 0;
+            for (int i = 0; i < items.Count; i++)
+            {
+                // 장비 장착 옵션
+                string equip = "미소유";
+                // 공격력 or 방어력
+                string AorD = "";
+
+                {
+                    // 방어 장비
+                    if (items[i].Defense > 0)
+                    {
+                        AorD = $"방어력 +{items[i].Defense}";
+                    }
+                    // 공격 장비
+                    else if (items[i].Attack > 0)
+                    {
+                        AorD = $"공격력 +{items[i].Attack}";
+                    }
+                    count++;
+                    // 장비 소유 유무
+
+                    if (!items[i].Own)
+                    {
+                        Console.WriteLine($" -{count} {items[i].Name} | {AorD} | {items[i].Description} | {equip}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($" -{count} {items[i].Name} | {AorD} | {items[i].Description} | {(int)((float)items[i].Gold * 0.85f)} G");
+                    }
+
+                }
+            }
         }
     }
 }
