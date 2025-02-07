285babe97e18c035beb3f668e3cf8cdd589871d3
S-SoHyun <slj506@naver.com>
2025-02-07 21:15:19 +0900

    [Docs] 0207 commit
    오늘까지 한 것 올립니다.
    아직 미구현 상태인 것이 많으며 주말동안 계속 수정해볼 계획입니다.

--- Window11_TextRPG/InventoryManager.cs
+++ Window11_TextRPG/InventoryManager.cs
@@ -5,23 +5,24 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Antlr4.Runtime;
+using Window11_TextRPG;
 
 namespace Window_11_TEXTRPG
 {
     public class InventoryManager : IScene
     {
-        private static InventoryManager Instance;    // 싱글톤 선언
-        private InventoryManager() {}    // 생성자
+        // Singleton
+        private InventoryManager() { }
+        private static InventoryManager? instance;
 
-        public static InventoryManager GetInstance()
+        InventoryManager Instance
         {
-            if (Instance == null)
+            get
             {
-                return new InventoryManager();
-            }
-            else
-            {
-                return Instance;
+                if (instance == null)
+                    instance = new InventoryManager();
+                return instance;
             }
         }
 
@@ -38,7 +39,6 @@ namespace Window_11_TEXTRPG
             mountableItems.Add(new MountableItem());
             mountableItems.Add(new MountableItem());
 
-
             //2. 포션 추가
             potions.Add(new PotionItem());
             potions.Add(new PotionItem());
@@ -46,60 +46,145 @@ namespace Window_11_TEXTRPG
         }
 
 
-
-        public void Equip()   // 장착
-        {
-
-        }
-
-
-        public void UnEquip()    // 장착 해제
-        {
-
-        }
-
-
-        public void UsePotion()    // 물약 사용
-        {
-            // 만약 물약을 사용하겠다하면
-                // 플레이어가 갖고 있는 물약 카운트가 줄고
-                // hp가 heal 됨
-
-            //if (potionItem[0] > 0)
-            //{
-            //    PotionItem.count--;
-            //    Player.hp += potionItem.heal;
-            //}
-        }
-
-        public void ViewEquipItem()    // 장착 아이템 보기
-        {
-
-        }
+        // ↓ 미구현. 로직짜면서 좀 더 깔끔하게 쓸 방법 고민중
 
         public void Enter()
         {
-            
+            //while (true)
+            //{
+
+            //    int result = UtilManager.PlayerInput(0, 1);
+            //    switch (result)
+            //    {
+            //        case 0:    // 로비로 나가기
+
+            //            break;
+            //        case 1:    // 장착 관리 창으로 가기
+            //            DisplayManager.EquipmentScene(player, ?);
+            //            break;
+            //        default:
+            //            Console.WriteLine("잘못된 입력입니다.");
+            //            break;
+            //    }
+            //    }
         }
 
 
 
+        //public void Equip(MountableItem select)
+        //{
+        //    if (select.Type = ITEMTYPE.WEAPON && select.Equip = true)
+        //    {
+        //        UnEquip(select);
+        //        select.Equip = true;
+        //    }
+        //    else
+        //    {
+        //        select.Equip = true;
+        //    }
 
-        // 무기 종류에 따라도 다르게 동작?
-        //만약 고른 게 종류가 무기라면
-        //    근데 그 고른 종류를 이미 장착했다면
-        //    이미 장착돼있는 건 빼기 
-        //    고른 걸 장착하기
-        //    장착이 안 돼있다면
-        //    고른 걸 장착하기
-        //만약 고른 게 종류가 아머라면
-        //    근데 그 고른 종류를 이미 장착했다면
-        //    이미 장착돼있는 건 빼기
-        //    고른 걸 장착하기
-        //    장착이 안 돼있다면
-        //    고른 걸 장착하기
-        // → 구현할 때 하나로 묶어서 정리
+        //    if (select.Type = ITEMTYPE.ARMOR && select.Equip = true)
+        //    {
+        //        UnEquip(select);
+        //        select.Equip = true;
+        //    }
+        //    else
+        //    {
+        //        select.Equip = true;
+        //    }
+        //}
 
+
+        //public void UnEquip(Item select)    // 장착 해제
+        //{
+        //    select.Equip = false;
+        //}
+
+
+        //public void UsePotion(PotionItem potion)    // 물약 사용
+        //{
+        //    if (potion.Count > 0)      // 만약 물약이 한 개 이상 있다면
+        //    {
+        //        potion[0].Count--;     // 플레이어가 갖고 있는 물약 카운트가 줄고
+        //        Player.hp += potion.heel;      // hp가 potion의 heal만큼 됨
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("보유한 포션이 부족합니다.");
+        //    }
+        //}
+
+
+        //public void ViewinventoryItem()    // 인벤토리 창에서 아이템 보기
+        //{
+        //    for (int i = 0; i < mountableItems.Count; i++)
+        //    {
+        //        Item select = mountableItems[i+1];
+        //        if (select.Equip)
+        //        {
+        //            Console.Write("[E] ");
+        //        }
+        //        Console.WriteLine($"{select.name} | {select.Type} +{select.?} | {select.Description}");
+        //    }
+        //}
+
+        //public void ViewEquipItem()    // 장착관리 창에서 아이템 보기
+        //{
+        //    for (int i = 0; i < mountableItems.Count; i++)
+        //    {
+        //        if (mountableItems[i].Equip)
+        //        {
+        //            Console.Write("[E] ");
+        //        }
+        //        Console.WriteLine($"{i + 1}. {mountableItems[i]}"); // 양식 다듬어서 (미완)
+        //    }
+        //}
+
+
+
+        // DisplayManager에 넣을 함수 //
+
+        //public void equipdisplay()
+        //{
+        //    Console.Write("[인벤토리]");
+
+        //    AddBlankLine();
+        //    Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+
+        //    AddBlankLine();
+        //    Console.WriteLine("[아이템 목록]");
+
+        //    AddBlankLine();
+        //    ViewEquipItem(items, true);
+
+        //    AddBlankLine(2);
+        //    Console.WriteLine("1. 아이템 구매");
+        //    Console.WriteLine("0. 나가기");
+
+        //    InputInduction();
+        //}
+
+
+
+        //public void equipmentdisplay()
+        //{
+        //    Console.Write("[인벤토리- 장착 관리]");
+
+        //    AddBlankLine();
+        //    Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+
+        //    AddBlankLine();
+        //    Console.WriteLine("[아이템 목록]");
+
+        //    AddBlankLine();
+        //    ViewEquipItem(items, true);    // 여기에서 앞에 번호 붙인 거 메서드 하나 따로 만들어야 될 듯? 아니면 좋은 방법 강구
+
+        //    AddBlankLine(2);
+        //    Console.WriteLine("0. 나가기");
+
+        //    InputInduction();
+        //}
     }
 }
+
 
