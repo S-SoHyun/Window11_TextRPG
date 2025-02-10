577ab886a0965518587af4149ca105eb69c5c100
김지현 <kjhstar2486@naver.com>
2025-02-10 15:20:25 +0900

    [add]QuestManager클래스추가

---
+++ Window11_TextRPG/QuestManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Window_11_TEXTRPG;
+
+namespace Window11_TextRPG
+{
+    internal class QuestManager : IScene
+    {
+        public void Enter()
+        {
+            
+        }
+    }
+}
