4db6f1cb14c60ac3b5553619ba0b4604faab33c0
S-SoHyun <slj506@naver.com>
2025-02-10 10:23:45 +0900

    [Refactor] 인벤토리 매니저 수정

--- Window11_TextRPG/InventoryManager.cs
+++ Window11_TextRPG/InventoryManager.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Antlr4.Runtime;
 using Window11_TextRPG;
 
 namespace Window_11_TEXTRPG
@@ -14,9 +13,9 @@ namespace Window_11_TEXTRPG
     {
         // Singleton
         private InventoryManager() { }
-        private static InventoryManager? instance;
+        public static InventoryManager? instance;
 
-        InventoryManager Instance
+        public static InventoryManager Instance
         {
             get
             {
@@ -31,7 +30,13 @@ namespace Window_11_TEXTRPG
         public List<MountableItem> mountableItems = new List<MountableItem>();
         public List<PotionItem> potions = new List<PotionItem>();
 
-      
+
+        // 장착 가능 아이템 - 웨폰, 아머를 나누기 위한 리스트 생성
+        List<MountableItem> weaponItems = new List<MountableItem>();
+        List<MountableItem> armorItems = new List<MountableItem>();
+
+
+
         public void AddItem()
         {
             // 1. 장착할 수 있는 아이템 추가
@@ -46,144 +51,204 @@ namespace Window_11_TEXTRPG
         }
 
 
-        // ↓ 미구현. 로직짜면서 좀 더 깔끔하게 쓸 방법 고민중
-
         public void Enter()
         {
-            //while (true)
-            //{
+            while (true)
+            {
+                int input = UtilManager.PlayerInput(0, 1);
 
-            //    int result = UtilManager.PlayerInput(0, 1);
-            //    switch (result)
-            //    {
-            //        case 0:    // 로비로 나가기
+                switch (input)
+                {
+                    case 0:
+                        DisplayManager.MainScene();
+                        break;
+                    case 1:
+                        //DisplayManager.EquipmentScene();
+                        break;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다.");
+                        break;
+                }
 
-            //            break;
-            //        case 1:    // 장착 관리 창으로 가기
-            //            DisplayManager.EquipmentScene(player, ?);
-            //            break;
-            //        default:
-            //            Console.WriteLine("잘못된 입력입니다.");
-            //            break;
-            //    }
-            //    }
+            }
+        }
+
+
+        // mountableItems 리스트에서 weapon이랑 armor 타입에 따라 나누기
+        public void DivisionType()
+        {
+            for (int i = 0; i < mountableItems.Count; i++)
+            {
+                if (mountableItems[i].Type == ITEMTYPE.WEAPON)
+                {
+                    weaponItems.Add(mountableItems[i]);
+                }
+                else if (mountableItems[i].Type == ITEMTYPE.ARMOR)
+                {
+                    armorItems.Add(mountableItems[i]);
+                }
+            }
+        }
+
+
+        // 코딩하다보니 종류별로 이큅되게 했는데 이렇게 하면 불편할 것 같음. equip, unequip으로 해야 되지 않을까?
+
+        public void WeaponEquip(int input)    // 웨폰아이템 장착
+        {
+            DivisionType();
+            MountableItem select = mountableItems[input - 1];
+            MountableItem equipped = null;
+
+            for (int i = 0; i < weaponItems.Count; i++)
+            {
+                if (weaponItems[i].Equip)
+                {
+                    equipped = weaponItems[i];
+                    break;
+                }
+            }
+
+            if (equipped == null)     // equipped가 여전히 null이라면 장착하고 아니면 그거 해제하고 고른거 장착하도록.
+            {
+                select.Equip = true;
+                //스탯창에서 atk, def 추가되는 코드
+            }
+            else
+            {
+                equipped.Equip = false;
+                select.Equip = true;
+                //스탯창에서 atk, def 추가되는 코드
+            }
+        }
+
+
+        public void ArmorEquip(int input)    // 아이템 장착
+        {
+            DivisionType();
+            MountableItem select = mountableItems[input - 1];
+            MountableItem equipped = null;
+
+            for (int i = 0; i < armorItems.Count; i++)
+            {
+                if (armorItems[i].Equip)
+                {
+                    equipped = armorItems[i];
+                    break;
+                }
+            }
+
+            if (equipped == null)     // equipped가 여전히 null이라면 장착하고 아니면 그거 해제하고 고른거 장착하도록.            
+                select.Equip = true;
+            else
+            {
+                equipped.Equip = false;
+                select.Equip = true;
+            }
+        }
+
+
+        public void UsePotion(PotionItem potion)    // 물약 사용
+        {
+            if (potion.Count > 0)      // 만약 물약이 한 개 이상 있다면
+            {
+                potions.RemoveAt(0);             // 물약 리스트에서 0번 항목 제거
+                //Player.hp += potion.heal;
+                Console.WriteLine("회복을 완료했습니다.");
+            }
+            else
+            {
+                Console.WriteLine("포션이 부족합니다.");
+            }
+        }
+
+
+        // ↓ DisplayManager에 넣을 함수 ↓ //
+
+        public void InventoryScene()
+        {
+            Console.Write("[인벤토리]");
+
+            Console.WriteLine();
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+
+            Console.WriteLine();
+            //ViewInventoryItem();    // DisplayManager?
+
+
+            Console.WriteLine();
+            Console.WriteLine("1. 아이템 구매");
+            Console.WriteLine("0. 나가기");
+
+            Console.WriteLine();
+        }
+
+        public void EquipmentScene()
+        {
+            Console.Write("[인벤토리- 장착 관리]");
+
+            Console.WriteLine();
+            Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+
+            Console.WriteLine();
+            Console.WriteLine("[아이템 목록]");
+
+            Console.WriteLine();
+            //ViewEquipItem();    // DisplayManager?
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+
+
+        }
+
+
+        public void ViewInventoryItem()     // 인벤토리 창에서 보일 아이템 목록
+        {
+            for (int i = 0; i < mountableItems.Count; i++)
+            {
+                string typeName = (mountableItems[i].Attack > 0) ? "공격력" : "방어력";
+                int value = (mountableItems[i].Attack > 0) ? mountableItems[i].Attack : mountableItems[i].Defense;
+
+                if (mountableItems[i].Equip)
+                {
+                    Console.Write("- [E]");
+                }
+                else
+                {
+                    Console.Write("- ");
+                }
+                Console.WriteLine($"{mountableItems[i].Name}  |  {typeName} + {value}  |  {mountableItems[i].Description}");
+            }
+        }
+
+
+        public void ViewEquipItem()    // 장착관리 창에서 보일 아이템 목록
+        {
+            for (int i = 0; i < mountableItems.Count; i++)
+            {
+                string typeName = (mountableItems[i].Attack > 0) ? "공격력" : "방어력";
+                int value = (mountableItems[i].Attack > 0) ? mountableItems[i].Attack : mountableItems[i].Defense;
+
+                if (mountableItems[i].Equip)
+                {
+                    Console.Write($"- {i + 1} [E]");
+                }
+                else
+                {
+                    Console.Write($"- {i + 1} ");
+                }
+                Console.WriteLine($"{mountableItems[i].Name}  |  {typeName} + {value}  |  {mountableItems[i].Description}");
+            }
         }
 
 
 
-        //public void Equip(MountableItem select)
-        //{
-        //    if (select.Type = ITEMTYPE.WEAPON && select.Equip = true)
-        //    {
-        //        UnEquip(select);
-        //        select.Equip = true;
-        //    }
-        //    else
-        //    {
-        //        select.Equip = true;
-        //    }
-
-        //    if (select.Type = ITEMTYPE.ARMOR && select.Equip = true)
-        //    {
-        //        UnEquip(select);
-        //        select.Equip = true;
-        //    }
-        //    else
-        //    {
-        //        select.Equip = true;
-        //    }
-        //}
 
 
-        //public void UnEquip(Item select)    // 장착 해제
-        //{
-        //    select.Equip = false;
-        //}
-
-
-        //public void UsePotion(PotionItem potion)    // 물약 사용
-        //{
-        //    if (potion.Count > 0)      // 만약 물약이 한 개 이상 있다면
-        //    {
-        //        potion[0].Count--;     // 플레이어가 갖고 있는 물약 카운트가 줄고
-        //        Player.hp += potion.heel;      // hp가 potion의 heal만큼 됨
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("보유한 포션이 부족합니다.");
-        //    }
-        //}
-
-
-        //public void ViewinventoryItem()    // 인벤토리 창에서 아이템 보기
-        //{
-        //    for (int i = 0; i < mountableItems.Count; i++)
-        //    {
-        //        Item select = mountableItems[i+1];
-        //        if (select.Equip)
-        //        {
-        //            Console.Write("[E] ");
-        //        }
-        //        Console.WriteLine($"{select.name} | {select.Type} +{select.?} | {select.Description}");
-        //    }
-        //}
-
-        //public void ViewEquipItem()    // 장착관리 창에서 아이템 보기
-        //{
-        //    for (int i = 0; i < mountableItems.Count; i++)
-        //    {
-        //        if (mountableItems[i].Equip)
-        //        {
-        //            Console.Write("[E] ");
-        //        }
-        //        Console.WriteLine($"{i + 1}. {mountableItems[i]}"); // 양식 다듬어서 (미완)
-        //    }
-        //}
-
-
-
-        // DisplayManager에 넣을 함수 //
-
-        //public void equipdisplay()
-        //{
-        //    Console.Write("[인벤토리]");
-
-        //    AddBlankLine();
-        //    Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
-
-        //    AddBlankLine();
-        //    Console.WriteLine("[아이템 목록]");
-
-        //    AddBlankLine();
-        //    ViewEquipItem(items, true);
-
-        //    AddBlankLine(2);
-        //    Console.WriteLine("1. 아이템 구매");
-        //    Console.WriteLine("0. 나가기");
-
-        //    InputInduction();
-        //}
-
-
-
-        //public void equipmentdisplay()
-        //{
-        //    Console.Write("[인벤토리- 장착 관리]");
-
-        //    AddBlankLine();
-        //    Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
-
-        //    AddBlankLine();
-        //    Console.WriteLine("[아이템 목록]");
-
-        //    AddBlankLine();
-        //    ViewEquipItem(items, true);    // 여기에서 앞에 번호 붙인 거 메서드 하나 따로 만들어야 될 듯? 아니면 좋은 방법 강구
-
-        //    AddBlankLine(2);
-        //    Console.WriteLine("0. 나가기");
-
-        //    InputInduction();
-        //}
     }
 }
 
