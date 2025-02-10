d270a55588b81d126af278d5709475f099e2d730
김지현 <kjhstar2486@naver.com>
2025-02-10 20:52:12 +0900

    [feat]Lobby에서 상점 이동 추가

--- Window11_TextRPG/LobbyManager.cs
+++ Window11_TextRPG/LobbyManager.cs
@@ -14,11 +14,12 @@ namespace Window11_TextRPG
         // 생성자
         private LobbyManager() 
         {
-            lobbyList = new string[5]
+            lobbyList = new string[6]
             {
                 "상태 보기",
                 "전투 시작",
                 "인벤토리",
+                "상점",
                 "퀘스트",
                 "저장하기",
             };
@@ -46,22 +47,26 @@ namespace Window11_TextRPG
             switch (input) 
             {
                 case 1:
-                    // 플레이어 Manger로 변환
+                    // 플레이어 Manger로 이동
                     GameManager.Instance.ChangeScene(SceneState.PlayerManager);
                     break;
                 case 2:
-                    // 던전 Manger로 변환
+                    // 던전 Manger로 이동
                     GameManager.Instance.ChangeScene(SceneState.DungeonManager);
                     break;
                 case 3:
-                    // 인벤토리 Manger로 변환
+                    // 인벤토리 Manger로 이동
                     GameManager.Instance.ChangeScene(SceneState.InventoryManager);
                     break;
                 case 4:
+                    // 상점으로 이동 
+                    GameManager.Instance.ChangeScene(SceneState.StoreManager);
+                    break;
+                case 5:
                     // 퀘스트 Manger로 변환 
                     GameManager.Instance.ChangeScene(SceneState.QuestManager);
                     break;
-                case 5:
+                case 6:
                     // 저장하기
                     GameManager.Instance.GetSave();
                     break;
