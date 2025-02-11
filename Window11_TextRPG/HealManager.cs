7588c20a34249f9259c3bd2da5562023dc65545c
S-SoHyun <slj506@naver.com>
2025-02-11 20:18:27 +0900

    [Feat] 힐매니저 추가

---
+++ Window11_TextRPG/HealManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Window_11_TEXTRPG;
+
+namespace Window11_TextRPG
+{
+    public class HealManager: IScene
+    {
+        // Singleton
+        private HealManager()
+        {
+        //    reward = new Reward(mountableItems, potion);
+        }
+        public static HealManager? instance;
+
+        public static HealManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new HealManager();
+                return instance;
+            }
+        }
+
+
+        public void Enter()
+        {
+            
+        }
+
+
+
+
+
+    }
+}
