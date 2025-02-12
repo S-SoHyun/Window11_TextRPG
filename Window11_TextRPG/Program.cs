bc11b2cc7dc518712a7c577cedc65021031f7f28
Lee Sanghun <hun1387s@gmail.com>
2025-02-12 11:44:29 +0900

    Revert "[fix] Gold 적용안되는 문제 수정"
    This reverts commit c36e01919c7188331e0149d9e3e2935bc47d6039.

--- Window11_TextRPG/Program.cs
+++ Window11_TextRPG/Program.cs
@@ -5,7 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            GameManager.Instance.ChangeScene(SceneState.PlayerManager);
         }
     }
 }
