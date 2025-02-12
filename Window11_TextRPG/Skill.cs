c4fadec2e410284192e7eba296cbc9fa0071870f
Lee Sanghun <hun1387s@gmail.com>
2025-02-12 14:10:56 +0900

    [feat] 스킬 객체 작성
    SkilManager에서 가져다 사용할 skill 객체.

--- Window11_TextRPG/Skill.cs
+++ Window11_TextRPG/Skill.cs
@@ -12,9 +12,18 @@ namespace Window11_TextRPG
         public string description { get; set; }
         public int mp { get; set; }
         public int type { get; set; }
-        Func<int> action { get; set; }
+        Func<Player, Monster, int> action { get; set; }
 
-        //action 함수 추가 필요
+        // action 포함 생성자
+        public Skill(string name, string description, int mp, int type, Func<Player, Monster, int> action)
+        {
+            this.name = name;
+            this.description = description;
+            this.mp = mp;
+            this.type = type;
+            this.action = action;
+        }
+        // action 미포함 생성자
         public Skill(string name, string description, int mp, int type)
         {
             this.name = name;
@@ -23,12 +32,28 @@ namespace Window11_TextRPG
             this.type = type;
         }
 
-        public void invoke()
+        // action을 나중에 설정 가능
+        public void SetAction(Func<Player, Monster, int> action)
         {
+            this.action = action;
+        }
+
+
+        public int Invoke(Player player, Monster monster)
+        {
+            int result = 0;
+
             if (action != null)
             {
-                action();
+                result = action(player, monster);
             }
+            else
+            {
+                Console.WriteLine("Action 값이 없다.");  // Debug용
+            }
+            return result;
         }
     }
+
+
 }
